e057957170f324e1ec3a92d5d0d01e295d843ab2
AndreiOsypchuk <andrewostin13@gmail.com>
2022-12-25 22:52:22 +0200

    fiddled with landing ui

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -7,7 +7,8 @@ import {
   IoMdClose,
 } from "react-icons/io";
 import { CgSpinner } from "react-icons/cg";
-import React, { ChangeEvent } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Ham = () => {
   const [openBurger, setOpenBurger] = React.useState(false);
@@ -41,7 +42,9 @@ const Ham = () => {
           <div className="flex  items-center py-4">
             <p className=" text-teal-600/60">Have the account?</p>
             <div className="m-3" />
-            <button className="underline text-teal-600/60">Sign In</button>
+            <Link to="/sign-in" className="underline text-teal-600/60">
+              Sign In
+            </Link>
           </div>
         </div>
       )}
@@ -50,7 +53,7 @@ const Ham = () => {
 };
 
 const wait = async (amount: number): Promise<void> => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, _reject) => {
     setTimeout(() => {
       console.log("fetched stuff");
       resolve();
@@ -69,13 +72,14 @@ export const Landing = () => {
   const handleSubmit = async (e: any) => {
     setLoading(() => true);
     e.preventDefault();
-    console.log(input);
     await wait(4000);
+    console.log(input);
+    setInput(() => "");
     setLoading(() => false);
   };
 
   return (
-    <div className="bg-indigo-900 h-auto flex flex-col justify-between relative">
+    <div className="bg-indigo-900 h-full flex flex-col justify-between relative">
       <nav className="z-20 py-6 px-6 flex justify-between items-center">
         <div className="flex items-center justify-between">
           {/* <Logo /> */}
@@ -85,7 +89,7 @@ export const Landing = () => {
         </div>
         <Ham />
       </nav>
-      <main className="px-6 h-screen">
+      <main className=" h-full">
         <div className="absolute rotate-180 w-full top-0 left-0 fill-white">
           <svg
             data-name="Layer 1"
@@ -100,8 +104,8 @@ export const Landing = () => {
             ></path>
           </svg>
         </div>
-        {/* TODO: Add image of laughing dude to the right idk */}
-        <section>
+
+        <section className="px-6 my-24 pb-12">
           <h1 className="text-white text-3xl font-bold">
             Link everything you are proud of
           </h1>
@@ -111,30 +115,69 @@ export const Landing = () => {
             Join over 15B+ people and link everything. All in one place. All in
             one link.
           </p>
+          <div className="m-12" />
+          <form className="flex justify-between" onSubmit={handleSubmit}>
+            <input
+              onChange={handleInput}
+              value={input}
+              className="h-12 px-3 py-1 rounded-full w-3/5"
+              placeholder="links.ea/yourhandle"
+            />
+            <button
+              disabled={loading || !input.length}
+              className="text-indigo-900 w-24 flex items-center justify-center font-semibold bg-teal-400 hover:bg-teal-300 px-4 rounded-lg"
+              type="submit"
+            >
+              {loading ? (
+                <CgSpinner className="animate-spin  text-teal-600 text-2xl" />
+              ) : (
+                "Lets go!"
+              )}
+            </button>
+          </form>
         </section>
 
-        <div className="m-12" />
-        <form className="flex justify-between" onSubmit={handleSubmit}>
-          <input
-            onChange={handleInput}
-            className="h-12 px-3 py-1 rounded-full w-3/5"
-            placeholder="links.ea/yourhandle"
-          />
-          <button
-            disabled={loading || !input.length}
-            className="text-indigo-900 w-24 flex items-center justify-center font-semibold bg-teal-400 hover:bg-teal-300 px-4 rounded-lg"
-            type="submit"
+        <section className="px-7   text-indigo-900 py-12 bg-white">
+          <h1 className="font-bold text-3xl mb-4">
+            Complete controll over{" "}
+            <span className="bg-indigo-900 text-teal-400 px-2">
+              how it looks!
+            </span>
+          </h1>
+          <p className="text-indigo-900 mb-12">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque rem
+            officia eum tempore facilis repellat aut libero, ad possimus commodi
+            deserunt. Iste nobis laborum ut cumque possimus, saepe sit
+            similique?
+          </p>
+          <Link
+            to="/register"
+            className="max-w-fit text-teal-400 h-12 flex items-center justify-center font-semibold bg-indigo-900 hover:bg-indigo-800 px-4 rounded-lg"
           >
-            {loading ? (
-              <CgSpinner className="animate-spin  text-teal-600 text-2xl" />
-            ) : (
-              "Lets go!"
-            )}
-          </button>
-        </form>
+            "Get started for free!"
+          </Link>
+        </section>
+        <section className="px-7   text-white py-12 ">
+          <h1 className="font-bold text-3xl mb-4">
+            Complete controll over{" "}
+            <span className="bg-teal-400 px-2">how it looks!</span>
+          </h1>
+          <p className="text-teal-400 mb-12">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque rem
+            officia eum tempore facilis repellat aut libero, ad possimus commodi
+            deserunt. Iste nobis laborum ut cumque possimus, saepe sit
+            similique?
+          </p>
+          <Link
+            to="/register"
+            className="max-w-fit text-white h-12 flex items-center justify-center font-semibold bg-teal-400 hover:bg-teal-300 px-4 rounded-lg"
+          >
+            "Get started for free!"
+          </Link>
+        </section>
       </main>
 
-      <footer className="relative z-50 p-5 text-indigo-300  bg-indigo-400/10 flex flex-col justify-center items-center  gap-4">
+      <footer className="relative z-50 p-5 mt-12 text-indigo-300  bg-indigo-400/10 flex flex-col justify-center items-center  gap-4">
         <div className="flex items-center justify-between w-3/6">
           <a href="https://github.com/AndreiOsypchuk/link_sea" target="_blank">
             <IoLogoGithub className="w-6 h-6 " />
@@ -148,27 +191,6 @@ export const Landing = () => {
         </div>
         <p className="text-xs">© 2022 Shyer Inc. All rights reserved.</p>
         <div className="z-30 absolute  w-full top-0 -translate-y-full  left-0 fill-indigo-400/10">
-          {/* <svg
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 1200 120"
-              preserveAspectRatio="none"
-            >
-              <path
-                d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
-                opacity=".25"
-                className="fill-indigo-400/10"
-              ></path>
-              <path
-                d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
-                opacity=".5"
-                className="fill-indigo-400/10"
-              ></path>
-              <path
-                d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
-                className="fill-indigo-400/10"
-              ></path>
-            </svg> */}
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
